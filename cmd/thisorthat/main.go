906ecc2192d98ff9c9df15d53f44eade10a280e3
wipdev-tech <wipdev.tech@gmail.com>
2024-03-02 17:55:50 +0200

    feat(main): initial server code and template
    Now we have a running server, handling env vars, and a hello world
    template.

--- cmd/thisorthat/main.go
+++ cmd/thisorthat/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type service struct {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 	s := service{}
 
 	fs := http.FileServer(http.Dir("static"))
@@ -19,9 +28,21 @@ func main() {
 	http.HandleFunc("/play", s.handlePlay)
 
 	fmt.Println("This or That Lang server, let's go!")
+	if os.Getenv("ENV") == "dev" {
+		fmt.Println("Dev server started and running at http://localhost:8080")
+		log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	}
+	fmt.Println("Server started and running")
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), nil))
 }
 
 func (s *service) handleHome(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles(
+		"templates/index.html",
+		// "templates/fragments.html",
+	))
+
+	tmpl.Execute(w, nil)
 }
 
 func (s *service) handleRegister(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module wipdev-tech/this-or-that-lang
 
 go 1.21.6
+
+require github.com/joho/godotenv v1.5.1
